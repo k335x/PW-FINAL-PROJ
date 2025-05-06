b949cbb8b62fe8ab067297b2ef67d1f4075c5c47
Yehor Hrebeniuk <hrebeniuk.e@gmail.com>
2025-05-06 08:55:38 +0300

    Fixed last comment

--- tests/auth.login.spec.ts
+++ tests/auth.login.spec.ts
@@ -2,7 +2,7 @@ import { test as test, expect } from '../helpers/fixtures';
 
 test('Login', async ({ homePage, loginPage, page}) => {
 
-    await page.goto('/auth/login');
+    await loginPage.navigateToLoginPage();
     await loginPage.login(process.env.USER_EMAIL!, process.env.USER_PASSWORD!);
     await expect(page).toHaveURL('/account');
     await expect(homePage.getPageTitleLocator()).toContainText('My account');
